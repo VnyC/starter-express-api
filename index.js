8ca052bbd4ffab5190d8607da47b286fea06446e
Vinay C <65414132+VnyC@users.noreply.github.com>
2023-05-11 16:41:40 +0530

    psql works

--- index.js
+++ index.js
@@ -1,7 +1,86 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo Vinay!')
-})
+const express = require('express');
+const knex = require('knex');
+const { json, urlencoded } = require('express');
+// const cors = require('cors');
+
+const app = express();
+
+app.use(urlencoded({type:false}));
+app.use(json());
+// app.use(cors());
+
+const pgdb = knex({
+    client: 'pg',
+    connection: {
+      connectionString: "postgres://profile:Vinayak17598@vinayakchawdimaniprofilepage.cpiqwwkm5tdd.ap-south-1.rds.amazonaws.com/postgres",
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
+
+
+
+app.get('/', (req, res) => {
+    res.send("It works 8, goto vinayakc.netlify.app");
+});
+
+app.get('/getall', (req, res) => {
+    pgdb('profilepage').then(data => {
+        res.json(data);
+    });
+});
+
+app.put('/putdata', (req, res) => {
+    var body = req.body;
+        pgdb('profile')
+        .where({head:body.head, type:body.type})
+        .update({
+            title:body.title,
+            subtitle:body.subtitle,
+            para1:body.para1,
+            para2:body.para2,
+            image:body.image,
+            icon:body.icon,
+            url:body.url
+        }, ['*']).then(data => {
+            res.send(data);
+        });
+});
+
+app.post('/postdata', (req, res) => {
+    var body = req.body;
+        pgdb('profile')
+        .insert({
+            head:body.head,
+            type:body.type,
+            title:body.title,
+            subtitle:body.subtitle,
+            para1:body.para1,
+            para2:body.para2,
+            image:body.image,
+            icon:body.icon,
+            url:body.url
+        }, ['*']).then(data => {
+            res.send(data);
+        });
+});
+
+app.get('/getdata', (req, res) => {
+    var head = req.query.head;
+    var type = req.query.type;
+    if (type == "*") {
+        var que = {head:head};
+      } else {
+        var que = {head:head,type:type};
+      }
+    pgdb('profilepage')
+        .where(que)
+        .then(data => {
+            res.json(data)
+        });
+});
+
+
+
 app.listen(process.env.PORT || 3000)
